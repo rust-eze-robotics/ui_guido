9550c2753957add1c17e5991b96e558baf2c7dd6
Davide Andreolli <dvd2003333@gmail.com>
2024-02-15 14:32:16 +0100

    chore: cleaned and documented player component

--- src/visualizer/components/player.rs
+++ src/visualizer/components/player.rs
@@ -1,27 +1,32 @@
-pub(crate) use ggez::context::Has;
+use ggez::context::Has;
 use ggez::glam::vec2;
-pub(crate) use ggez::graphics::GraphicsContext;
-pub(crate) use ggez::graphics::Image;
+use ggez::graphics::GraphicsContext;
+use ggez::graphics::Image;
 
 use crate::visualizer::textures::Texture;
 
 use super::Component;
 
-pub(crate) struct PlayerComponent {
+/// The structure contains the state of the player component.
+pub(in crate::visualizer) struct PlayerComponent {
     image: Image,
     position: (usize, usize),
     map_size: (usize, usize),
 }
 
-pub(crate) struct PlayerComponentParam {
-    pub(crate) scale: f32,
+/// The structure contains parameters required by draw function.
+pub(in crate::visualizer) struct PlayerComponentParam {
+    scale: f32,
 }
 
-pub(crate) struct PlayerComponentUpdateParam {
-    pub(crate) position: (usize, usize),
+/// The structure contains parameters required by update function.
+pub(in crate::visualizer) struct PlayerComponentUpdateParam {
+    position: (usize, usize),
 }
 
 impl PlayerComponent {
+
+    /// The constructor creates a new instance of the player component.
     pub(crate) fn new(
         gfx: &impl Has<GraphicsContext>,
         initial_position: (usize, usize),
@@ -33,39 +38,26 @@ impl PlayerComponent {
             map_size,
         }
     }
-
-    pub(crate) fn texture() -> Texture {
-        Texture::Player
-    }
-
-    pub(crate) fn position(&self) -> (usize, usize) {
-        self.position
-    }
 }
 
 impl Component<PlayerComponentParam, PlayerComponentUpdateParam> for PlayerComponent {
+
     fn draw(
         &self,
         canvas: &mut ggez::graphics::Canvas,
         draw_param: ggez::graphics::DrawParam,
         component_param: PlayerComponentParam,
     ) -> Result<(), ggez::GameError> {
-        let y = self.position.0 as usize;
-        let x = self.position.1 as usize;
-        let scale = component_param.scale;
-        // let player_x = (self.image.width() as f32 * 0.5)
-        //     * (map_size_y as usize - y + x - 1) as f32;
-        // let player_y = ((self.image.height() as f32 - 1.0) * 0.25) * (x + y) as f32;
 
-        // println!("PlayerComponent::draw: x: {}, y: {}", x, y);
-        // println!("Map size: {:?}", self.map_size);
-        // println!("Scale: {}", scale);
+        let y = self.position.0 as usize;   // row
+        let x = self.position.1 as usize;   // column
+        let scale = component_param.scale;
 
         let player_x =
             (self.image.width() as f32 * 0.5) * (self.map_size.1 as usize - y + x - 1) as f32;
         let player_y = ((Texture::height() - 1.0) * 0.25) * (x + y) as f32;
 
-        // println!("PlayerComponent::draw: player_x: {}, player_y: {}", player_x, player_y);
+        // Draws the player component.
         canvas.draw(
             &self.image,
             draw_param
@@ -76,18 +68,24 @@ impl Component<PlayerComponentParam, PlayerComponentUpdateParam> for PlayerCompo
     }
 
     fn update(&mut self, update_param: PlayerComponentUpdateParam) -> Result<(), ggez::GameError> {
+        
+        // Updates the position of the player component.
         self.position = update_param.position;
         Ok(())
     }
 }
 
 impl PlayerComponentParam {
+
+    /// The constructor creates a new instance of the player component parameters.
     pub(crate) fn new(scale: f32) -> Self {
         Self { scale }
     }
 }
 
 impl PlayerComponentUpdateParam {
+
+    /// The constructor creates a new instance of the player component update parameters.
     pub(crate) fn new(position: (usize, usize)) -> Self {
         Self { position }
     }
